5872a2233f61a7b041cc3f77b7e3e3639ce5f55a
RenardFute <axel.eckenberg@yahoo.fr>
2024-03-16 13:36:47 +0100

    :sparkles: Add all makefile and first source files The source files are just here for the compilation you can keep or discard them

---
+++ games/Pacman/src/Pacman.hpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2024
+** Arcade
+** File description:
+** No file there , just an epitech header example .
+** You can even have multiple lines if you want !
+*/
+
+#pragma once
+#include "shared/IGame.hpp"
+
+class Pacman: public IGame {
+
+};
