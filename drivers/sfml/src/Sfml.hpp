83343043679f8184e3df67199b9edf082c0b7602
RenardFute <axel.eckenberg@yahoo.fr>
2024-03-18 10:01:38 +0100

    :sparkles: Add makefile and directory for sfml

---
+++ drivers/sfml/src/Sfml.hpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2024
+** Arcade
+** File description:
+** No file there , just an epitech header example .
+** You can even have multiple lines if you want !
+*/
+
+#pragma once
+#include "shared/IDriver.hpp"
+
+class SFML: public IDriver {
+
+};
