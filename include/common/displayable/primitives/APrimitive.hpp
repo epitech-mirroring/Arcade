ae54e9e71c9a6e8cd39f264ec6c7b4eea2ab5ee7
RenardFute <axel.eckenberg@yahoo.fr>
2024-03-25 17:22:02 +0100

    :sparkles: Implement IPrimitive

---
+++ include/common/displayable/primitives/APrimitive.hpp
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2024
+** Arcade
+** File description:
+** No file there , just an epitech header example .
+** You can even have multiple lines if you want !
+*/
+
+#pragma once
+#include "../../../shared/displayable/primitives/IPrimitive.hpp"
+
+class APrimitive : public IPrimitive {
+protected:
+    std::unique_ptr<IColor> _color;
+    APrimitive(std::unique_ptr<IColor> color);
+public:
+    ~APrimitive() override = default;
+    const std::unique_ptr<IColor> &getColor() const override;
+};
---
+++ libs/common/displayable/primitives/APrimitive.cpp
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2024
+** Arcade
+** File description:
+** No file there , just an epitech header example .
+** You can even have multiple lines if you want !
+*/
+
+#include "common/displayable/primitives/APrimitive.hpp"
+
+APrimitive::APrimitive(std::unique_ptr<IColor> color)
+{
+    _color = std::move(color);
+}
+
+const std::unique_ptr<IColor> &APrimitive::getColor() const
+{
+    return _color;
+}
