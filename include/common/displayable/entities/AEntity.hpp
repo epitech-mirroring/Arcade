2f7ad63356e7f89e21463e8709fbd1e68c72a7f0
RenardFute <axel.eckenberg@yahoo.fr>
2024-03-25 17:14:26 +0100

    :sparkles: Implement IEntity

---
+++ include/common/displayable/entities/AEntity.hpp
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2024
+** Arcade
+** File description:
+** No file there , just an epitech header example .
+** You can even have multiple lines if you want !
+*/
+
+#pragma once
+#include "../../../shared/displayable/entities/IEntity.hpp"
+
+class AEntity : public IEntity {
+protected:
+    std::unique_ptr<ISprite> _sprite;
+    AEntity(std::unique_ptr<ISprite> sprite);
+    AEntity(const std::string &path);
+
+public:
+    ~AEntity() = default;
+    const std::unique_ptr<ISprite> &getSprite() const override;
+    void setSprite(ISprite &sprite) override;
+};
