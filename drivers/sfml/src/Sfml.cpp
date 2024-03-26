f4fa63104a459d6755279401d1f371b1af0d534b
Marius-Pn1 <marius.pain@epitech.eu>
2024-03-26 18:16:28 +0100

    :construction: Add SFML driver implementation

--- drivers/sfml/src/Sfml.cpp
+++ drivers/sfml/src/Sfml.cpp
@@ -7,3 +7,63 @@
 */
 
 #include "Sfml.hpp"
+#include <iostream>
+
+extern "C" {
+    __attribute__((constructor))
+    void load_lib()
+    {
+        std::cout << "Loading SFML driver" << std::endl;
+    }
+
+    __attribute__((destructor))
+    void unload_lib()
+    {
+        std::cout << "Unloading SFML driver" << std::endl;
+    }
+
+    std::unique_ptr<IDriver> create_driver()
+    {
+        std::unique_ptr<IDriver> driver = std::make_unique<SFML>();
+        return driver;
+    }
+
+    const std::string &get_name()
+    {
+        static const std::string driver_name = "SFML";
+        return driver_name;
+    }
+}
+
+SFML::SFML()
+{
+    this->_window.create(sf::VideoMode(800, 600), "Arcade");
+    this->_window.setFramerateLimit(60);
+    this->_window.clear();
+}
+
+SFML::~SFML()
+{
+    this->_window.close();
+}
+
+void SFML::display(std::shared_ptr<IDisplayable> displayable)
+{
+    sf::RectangleShape shape(sf::Vector2f(displayable->getSize(), displayable->getSize()));
+
+    shape.setPosition(displayable->getPosition()->getX(), displayable->getPosition()->getY());
+    shape.setFillColor(sf::Color::Red);
+    this->_window.draw(shape);
+}
+
+void SFML::flipFrame()
+{
+    this->_window.display();
+    this->_window.clear();
+}
+
+void SFML::bindEvent(IEvent::EventType type, EventKey key, EventCallback callback)
+{
+    (void) type;
+    this->_events[key] = callback;
+}
--- drivers/sfml/src/Sfml.hpp
+++ drivers/sfml/src/Sfml.hpp
@@ -8,7 +8,21 @@
 
 #pragma once
 #include "shared/IDriver.hpp"
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Audio.hpp>
 
 class SFML: public IDriver {
+public:
+    SFML();
+    ~SFML() override;
+    void display(std::shared_ptr<IDisplayable> displayable) override;
+    void flipFrame() override;
+    void bindEvent(IEvent::EventType type, EventKey key, EventCallback callback) override;
+
+private:
+    sf::RenderWindow _window;
+    std::map<EventKey, EventCallback> _events;
 
 };
