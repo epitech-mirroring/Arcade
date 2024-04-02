9104e96357dc29f8cd0202edde01ae3b98cf84d1
Marius-Pn1 <marius.pain@epitech.eu>
2024-04-02 16:34:06 +0200

    :zap: Update SDL2Exception class constructor

--- drivers/sdl2/src/SDL2Exception.hpp
+++ drivers/sdl2/src/SDL2Exception.hpp
@@ -12,12 +12,10 @@
 class SDL2Exception : public IDriverError {
 private:
     std::string _message;
-    std::shared_ptr<IDriver> _driver;
 public:
-    SDL2Exception(std::string const &message, IDriver &driver);
+    SDL2Exception(std::string const &message);
     ~SDL2Exception() override = default;
 
     [[nodiscard]] const char *what() const noexcept override;
     [[nodiscard]] int getCode() const override;
-    [[nodiscard]] const IDriver &getDriver() const override;
 };
