6b5c665233c5343e6255e12236f2ac532d2594b1
Marius-Pn1 <marius.pain@epitech.eu>
2024-04-02 16:35:33 +0200

    :sparkles: Add NCurses driver

--- drivers/ncurses/src/NCurses.cpp
+++ drivers/ncurses/src/NCurses.cpp
@@ -37,17 +37,17 @@ extern "C" {
 }
 
 NCurses::NCurses() {
-    this->_preferredHeight = 500;
-    this->_preferredWidth = 500;
-    this->_window = std::unique_ptr<WINDOW, decltype(&delwin)>(initscr(), &delwin);
-    if (this->_window == nullptr)
-        throw std::runtime_error("Failed to initialize ncurses window");
-    keypad(this->_window.get(), TRUE);
+    this->_width = 500 / SCALE_WIDTH;
+    this->_height = 500 / SCALE_HEIGHT;
+    initscr();
+    if (stdscr == nullptr)
+        throw NCursesException("Failed to initialize ncurses");
+    keypad(stdscr, TRUE);
     noecho();
     curs_set(0);
-    nodelay(this->_window.get(), TRUE);
+    nodelay(stdscr, TRUE);
     start_color();
-    init_pair(1, COLOR_BLACK, COLOR_WHITE);
+    init_pair(1, COLOR_BLACK, COLOR_BLACK);
     init_pair(2, COLOR_RED, COLOR_BLACK);
     init_pair(3, COLOR_GREEN, COLOR_BLACK);
     init_pair(4, COLOR_YELLOW, COLOR_BLACK);
@@ -55,6 +55,81 @@ NCurses::NCurses() {
     init_pair(6, COLOR_MAGENTA, COLOR_BLACK);
     init_pair(7, COLOR_CYAN, COLOR_BLACK);
     init_pair(8, COLOR_WHITE, COLOR_BLACK);
+    this->_keyMap = {
+        {'a', _KEY_A},
+        {'b', _KEY_B},
+        {'c', _KEY_C},
+        {'d', _KEY_D},
+        {'e', _KEY_E},
+        {'f', _KEY_F},
+        {'g', _KEY_G},
+        {'h', _KEY_H},
+        {'i', _KEY_I},
+        {'j', _KEY_J},
+        {'k', _KEY_K},
+        {'l', _KEY_L},
+        {'m', _KEY_M},
+        {'n', _KEY_N},
+        {'o', _KEY_O},
+        {'p', _KEY_P},
+        {'q', _KEY_Q},
+        {'r', _KEY_R},
+        {'s', _KEY_S},
+        {'t', _KEY_T},
+        {'u', _KEY_U},
+        {'v', _KEY_V},
+        {'w', _KEY_W},
+        {'x', _KEY_X},
+        {'y', _KEY_Y},
+        {'z', _KEY_Z},
+        {'1', _KEY_DIGIT_1},
+        {'&', _KEY_DIGIT_1},
+        {'2', _KEY_DIGIT_2},
+        {169, _KEY_DIGIT_2},
+        {'3', _KEY_DIGIT_3},
+        {'"', _KEY_DIGIT_3},
+        {'4', _KEY_DIGIT_4},
+        {'\'', _KEY_DIGIT_4},
+        {'5', _KEY_DIGIT_5},
+        {'(', _KEY_DIGIT_5},
+        {'6', _KEY_DIGIT_6},
+        {'-', _KEY_DIGIT_6},
+        {'7', _KEY_DIGIT_7},
+        {168, _KEY_DIGIT_7},
+        {'8', _KEY_DIGIT_8},
+        {'_', _KEY_DIGIT_8},
+        {'9', _KEY_DIGIT_9},
+        {167, _KEY_DIGIT_9},
+        {'0', _KEY_DIGIT_0},
+        {160, _KEY_DIGIT_0},
+        {KEY_F(1), _KEY_F1},
+        {KEY_F(2), _KEY_F2},
+        {KEY_F(3), _KEY_F3},
+        {KEY_F(4), _KEY_F4},
+        {KEY_F(5), _KEY_F5},
+        {KEY_F(6), _KEY_F6},
+        {KEY_F(7), _KEY_F7},
+        {KEY_F(8), _KEY_F8},
+        {KEY_F(9), _KEY_F9},
+        {KEY_F(10), _KEY_F10},
+        {KEY_F(11), _KEY_F11},
+        {KEY_F(12), _KEY_F12},
+        {KEY_UP, _KEY_UP},
+        {KEY_DOWN, _KEY_DOWN},
+        {KEY_LEFT, _KEY_LEFT},
+        {KEY_RIGHT, _KEY_RIGHT},
+        {KEY_BACKSPACE, _KEY_BACKSPACE},
+        {KEY_ENTER, _KEY_ENTER},
+        {' ', _KEY_SPACE},
+        {27, _KEY_ESCAPE},
+        {9, _KEY_TAB},
+    };
+}
+
+NCurses::~NCurses() {
+    endwin();
+    this->_events.clear();
+    this->_frameBuffer.clear();
 }
 
 static int getNcursesColor(const IColor &color) {
@@ -81,10 +156,28 @@ static int getNcursesColor(const IColor &color) {
     return 8;
 }
 
+void NCurses::display(const IDisplayable &displayable) {
+    if (IS_INSTANCE_OF(const IPrimitive, displayable)) {
+        displayPrimitive(dynamic_cast<const IPrimitive &>(displayable));
+    } else if (IS_INSTANCE_OF(const IEntity, displayable)) {
+        displayEntity(dynamic_cast<const IEntity &>(displayable));
+    }
+}
+
+void NCurses::displayPrimitive(const IPrimitive &primitive) {
+    if (IS_INSTANCE_OF(const IText, primitive)) {
+        displayText(dynamic_cast<const IText &>(primitive));
+    } else if (IS_INSTANCE_OF(const ICircle, primitive)) {
+        displayCircle(dynamic_cast<const ICircle &>(primitive));
+    } else if (IS_INSTANCE_OF(const ISquare, primitive)) {
+        displaySquare(dynamic_cast<const ISquare &>(primitive));
+    }
+}
+
 void NCurses::displayText(const IText &text) {
     int color = getNcursesColor(text.getColor());
-    std::size_t x = text.getPosition().getX();
-    std::size_t y = text.getPosition().getY();
+    std::size_t x = text.getPosition().getX() / SCALE_WIDTH;
+    std::size_t y = text.getPosition().getY() / SCALE_HEIGHT;
 
     for (std::size_t i = 0; i < text.getText().size(); i++) {
         this->_frameBuffer[x + i][y] = BufferedChar{text.getText()[i], color};
@@ -92,13 +185,13 @@ void NCurses::displayText(const IText &text) {
 }
 
 void NCurses::displayCircle(const ICircle &circle) {
-    std::size_t radius = circle.getRadius();
-    std::size_t centerX = circle.getPosition().getX() + radius;
-    std::size_t centerY = circle.getPosition().getY() + radius;
-    std::size_t startX = circle.getPosition().getX();
-    std::size_t startY = circle.getPosition().getY();
-    std::size_t endX = circle.getPosition().getX() + radius * 2;
-    std::size_t endY = circle.getPosition().getY() + radius * 2;
+    std::size_t radius = circle.getRadius() / SCALE_WIDTH;
+    std::size_t centerX = circle.getPosition().getX() / SCALE_WIDTH + radius;
+    std::size_t centerY = circle.getPosition().getY() / SCALE_HEIGHT + radius;
+    std::size_t startX = circle.getPosition().getX() / SCALE_WIDTH;
+    std::size_t startY = circle.getPosition().getY() / SCALE_HEIGHT;
+    std::size_t endX = circle.getPosition().getX() / SCALE_WIDTH + radius * 2;
+    std::size_t endY = circle.getPosition().getY() / SCALE_HEIGHT + radius * 2;
     int color = getNcursesColor(circle.getColor());
 
     for (std::size_t y = startY; y < endY; y++) {
@@ -113,74 +206,106 @@ void NCurses::displayCircle(const ICircle &circle) {
 
 void NCurses::displaySquare(const ISquare &square) {
     int color = getNcursesColor(square.getColor());
+    std::size_t startX = square.getPosition().getX() / SCALE_WIDTH;
+    std::size_t startY = square.getPosition().getY() / SCALE_HEIGHT;
+    std::size_t endX = square.getPosition().getX() / SCALE_WIDTH + square.getWidth() * square.getSize() / SCALE_WIDTH;
+    std::size_t endY = square.getPosition().getY() / SCALE_HEIGHT + square.getHeight() * square.getSize() / SCALE_HEIGHT;
 
-    for (std::size_t y = square.getPosition().getY(); y < square.getHeight(); y++) {
-        for (std::size_t x = square.getPosition().getX(); x < square.getWidth(); x++) {
-            // Draw hollow square
-            if (x == 0 || y == 0 || y == square.getHeight() - 1 || x == square.getWidth() - 1) {
+    for (std::size_t y = startY; y < endY; y++) {
+        for (std::size_t x = startX; x < endX; x++) {
+            if (x == startX || y == startY || y == endY - 1 || x == endX - 1) {
                 this->_frameBuffer[x][y] = BufferedChar{square.getReplacingChar(), color};
             }
         }
     }
 }
 
-void NCurses::displayPrimitive(const IPrimitive &primitive) {
-    if (INSTANCE_OF(const IText, primitive)) {
-        displayText(dynamic_cast<const IText &>(primitive));
-    } else if (INSTANCE_OF(const ICircle, primitive)) {
-        displayCircle(dynamic_cast<const ICircle &>(primitive));
-    } else if (INSTANCE_OF(const ISquare, primitive)) {
-        displaySquare(dynamic_cast<const ISquare &>(primitive));
-    }
-}
-
 void NCurses::displayEntity(const IEntity &entity) {
-    // display entity
-}
+    if (entity.getReplacingChar() == ' ' || entity.getSize() == 0)
+        return;
+    int color = getNcursesColor((IColor &)RGBAColor::BLACK);
+    std::size_t posX = entity.getPosition().getX() / SCALE_WIDTH;
+    std::size_t posY = entity.getPosition().getY() / SCALE_HEIGHT;
+    std::size_t width = (entity.getSprite().getPicture().getWidth() * entity.getSize()) / SCALE_WIDTH;
+    std::size_t height = (entity.getSprite().getPicture().getHeight() * entity.getSize()) / SCALE_HEIGHT;
 
-void NCurses::display(const IDisplayable &displayable) {
-    if (INSTANCE_OF(const IPrimitive, displayable)) {
-        displayPrimitive(dynamic_cast<const IPrimitive &>(displayable));
-    } else if (INSTANCE_OF(const IEntity, displayable)) {
-        displayEntity(dynamic_cast<const IEntity &>(displayable));
+    for (std::size_t y = 0; y < height; y++) {
+        for (std::size_t x = 0; x < width; x++) {
+            this->_frameBuffer[posX + x][posY + y] = BufferedChar{entity.getReplacingChar(), color};
+        }
     }
 }
 
 void NCurses::flipFrame() {
-    if (this->_window == nullptr)
-        throw std::runtime_error("Window is null");
-    std::size_t width = getmaxx(this->_window.get());
-    std::size_t height = getmaxy(this->_window.get());
+    if (stdscr == nullptr)
+        throw NCursesException("Window is null");
+    std::size_t width = getmaxx(stdscr);
+    std::size_t height = getmaxy(stdscr);
 
-    if (width < this->_preferredWidth || height < this->_preferredHeight) {
-        mvwprintw(this->_window.get(), 0, 0, "Window is too small");
-        wrefresh(this->_window.get());
+    if (width < this->_width || height < this->_height) {
+        for (std::size_t x = 0; x < width; x++) {
+            for (std::size_t y = 0; y < height; y++) {
+                mvaddch(y, x, ' ');
+            }
+        }
+        mvwprintw(stdscr, 0, 0, "Window is too small");
+        wrefresh(stdscr);
+        this->_frameBuffer.clear();
+        this->handleEvents();
         return;
     }
 
-    for (std::size_t x = 0; x < this->_preferredWidth; x++) {
-        for (std::size_t y = 0; y < this->_preferredHeight; y++) {
+    for (std::size_t x = 0; x < this->_width - 2; x++) {
+        for (std::size_t y = 0; y < this->_height - 2; y++) {
             if (this->_frameBuffer.find(x) != this->_frameBuffer.end() &&
                 this->_frameBuffer[x].find(y) != this->_frameBuffer[x].end()) {
-                wattron(this->_window.get(), COLOR_PAIR(this->_frameBuffer[x][y].color));
-                mvwaddch(this->_window.get(), y, x, this->_frameBuffer[x][y].c);
-                wattroff(this->_window.get(), COLOR_PAIR(this->_frameBuffer[x][y].color));
+                wattron(stdscr, COLOR_PAIR(this->_frameBuffer[x][y].color));
+                mvwaddch(stdscr, y + 1, x + 1, this->_frameBuffer[x][y].c);
+                wattroff(stdscr, COLOR_PAIR(this->_frameBuffer[x][y].color));
             } else {
-                mvwaddch(this->_window.get(), y, x, ' ');
+                mvwaddch(stdscr, y + 1, x + 1, ' ');
             }
         }
     }
-    wrefresh(this->_window.get());
+    mvhline(0, 0, 0, this->_width);
+    mvhline(this->_height - 1, 0, 0, this->_width);
+    mvvline(0, 0, 0, this->_height);
+    mvvline(0, this->_width - 1, 0, this->_height);
+    wrefresh(stdscr);
     this->_frameBuffer.clear();
+    this->handleEvents();
 }
 
 void NCurses::bindEvent(IEvent::EventType type, EventKey key, EventCallback callback) {
-    if (type == IEvent::MOUSE_DOWN || type == IEvent::MOUSE_UP || type == IEvent::MOUSE_MOVE)
+    if (type == IEvent::_MOUSE_DOWN || type == IEvent::_MOUSE_UP || type == IEvent::_MOUSE_MOVE)
         return;
-    this->_events[key] = callback;
+    this->_events[std::make_pair(type, key)] = callback;
+}
+
+void NCurses::unbindAll() {
+    this->_events.clear();
 }
 
 void NCurses::setPreferredSize(std::size_t width, std::size_t height) {
-    this->_preferredWidth = width;
-    this->_preferredHeight = height;
+    this->_width = width / SCALE_WIDTH + 2;
+    this->_height = height / SCALE_HEIGHT + 2;
+}
+
+void NCurses::handleEvents() {
+    int event = wgetch(stdscr);
+
+    if (event == ERR)
+        return;
+    handleInput(event);
+}
+
+void NCurses::handleInput(int event)
+{
+    if (this->_keyMap.find(event) == this->_keyMap.end())
+        return;
+
+    EventCallback callback = this->_events[std::make_pair(IEvent::EventType::_KEY_DOWN, this->_keyMap[event])];
+    if (callback) {
+        callback(Event(IEvent::EventType::_KEY_DOWN, this->_keyMap[event]));
+    }
 }
--- drivers/ncurses/src/NCurses.hpp
+++ drivers/ncurses/src/NCurses.hpp
@@ -7,42 +7,54 @@
 */
 
 #pragma once
-#include "shared/IDriver.hpp"
-#include <unordered_map>
 #include <ncurses.h>
-#include <string>
+#include "shared/IDriver.hpp"
+#include "shared/IDriver.hpp"
 #include "shared/displayable/primitives/IPrimitive.hpp"
 #include "shared/displayable/primitives/IText.hpp"
 #include "shared/displayable/primitives/ICircle.hpp"
 #include "shared/displayable/primitives/ISquare.hpp"
 #include "shared/displayable/entities/IEntity.hpp"
+#include "shared/utils/ICanRotate.hpp"
+#include "common/displayable/ADisplayable.hpp"
+#include "common/events/Event.hpp"
+#include "common/utils/RGBAColor.hpp"
+#include "NCursesException.hpp"
+#include <map>
+#include <deque>
+#include <cstddef>
+#include <memory>
+#include <iostream>
 
-#define INSTANCE_OF(type, obj) (dynamic_cast<type *>(&obj) != nullptr)
+#define SCALE_WIDTH 8
+#define SCALE_HEIGHT 18
 
 struct BufferedChar {
     char c;
     int color;
 };
-
 class NCurses: public IDriver {
-protected:
-    std::size_t _preferredWidth;
-    std::size_t _preferredHeight;
-    std::unordered_map<EventKey, EventCallback> _events;
-    std::unique_ptr<WINDOW, decltype(&delwin)> _window = std::unique_ptr<WINDOW, decltype(&delwin)>(nullptr, &delwin);
-    std::unordered_map<std::size_t, std::unordered_map<std::size_t, BufferedChar>> _frameBuffer;
 public:
     NCurses();
-    ~NCurses() override = default;
-
+    ~NCurses() override;
     void display(const IDisplayable &displayable) override;
     void flipFrame() override;
     void bindEvent(IEvent::EventType type, EventKey key, EventCallback callback) override;
     void setPreferredSize(std::size_t width, std::size_t height) override;
+    void unbindAll() override;
 
+private:
+    void handleEvents();
+    void handleInput(int event);
     void displayText(const IText &text);
     void displaySquare(const ISquare &square);
     void displayCircle(const ICircle &circle);
     void displayEntity(const IEntity &entity);
     void displayPrimitive(const IPrimitive &primitive);
+
+    std::map<std::pair<IEvent::EventType, EventKey>, EventCallback> _events;
+    std::map<int, EventKey> _keyMap;
+    std::size_t _width;
+    std::size_t _height;
+    std::unordered_map<std::size_t, std::unordered_map<std::size_t, BufferedChar>> _frameBuffer;
 };
